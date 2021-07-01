42540caab311a135a067e458e37d3b4554a9702c
grzany <robert.grzankowski@gmail.com>
2021-07-01 07:13:51 +0100

    progress

--- list-ops/listops.go
+++ list-ops/listops.go
@@ -2,6 +2,8 @@
 package listops
 
 type binFunc = func(x, y int) int
+type predFunc = func(x int) bool
+type unaryFunc = func(x int) int
 
 //IntList defines type of list of ints
 type IntList []int
@@ -9,20 +11,70 @@ type IntList []int
 // Foldr inplements right fold
 func (list IntList) Foldr(fun binFunc, i int) int {
 
-	return 0
+	res := i
+	for k := len(list) - 1; k >= 0 && len(list) > 0; k-- {
+		if res != 0 {
+			res = fun(list[k], res)
+		}
+	}
+	return res
 }
 
-// Foldl inplements right fold
+// Foldl inplements left fold
 func (list IntList) Foldl(fun binFunc, i int) int {
-
-	return 0
+	res := i
+	for _, ll := range list {
+		if res != 0 {
+			res = fun(ll, res)
+		}
+	}
+	return res
 }
 
 // Length returns length of the list
 func (list IntList) Length() int {
-	c := 0
-	for _, i := range list {
-		c++
+	return len(list)
+}
+
+// Filter returns int list filtering on pred function
+func (list IntList) Filter(fun predFunc) IntList {
+	l := IntList{}
+	for _, ll := range list {
+		if fun(ll) {
+			l = append(l, ll)
+		}
 	}
-	return c
+	return l
+}
+
+// Map returns int list with fun applied on list elements
+func (list IntList) Map(fun unaryFunc) IntList {
+	l := IntList{}
+	for _, ll := range list {
+		l = append(l, fun(ll))
+	}
+	return l
+}
+
+// Reverse returns reversed list
+func (list IntList) Reverse() IntList {
+	ll := IntList{}
+	for i := len(list) - 1; i >= 0 && len(list) > 0; i-- {
+		ll = append(ll, list[i])
+	}
+	return ll
+}
+
+// Append adds all elements of l to the list
+func (list IntList) Append(l IntList) IntList {
+	return append(list, l...)
+
+}
+
+// Concat adds to the lists all lists from l
+func (list IntList) Concat(l []IntList) IntList {
+	for _, ll := range l {
+		list = list.Append(ll)
+	}
+	return list
 }
